ac77e08f79c0cc7ae74269c19d177ca6865cdbdf
melona <71018719+OlttaeMelona@users.noreply.github.com>
2022-11-01 20:16:03 +0900

    [은영] ocr 입점문의

--- othub/src/main/java/reserv/ReserveController.java
+++ othub/src/main/java/reserv/ReserveController.java
@@ -57,6 +57,7 @@ public class ReserveController {
 		r_service.deleteTemporarily2(m_id);
 		List<ReservDTO> myr_list = r_service.myReservList(m_id);
 		model.addAttribute("myList", myr_list);
+		System.out.println();
 		return "reserv/myreserv";
 	}
 	
